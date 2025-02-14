4550f150f23cd1f4698fbba10df993d229f3aed9
Sushant Gandhi <sushant@simplified.co>
2025-02-14 10:22:44 +0530

    switched off some other eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,8 @@ const eslintConfig = [
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": ["off"],
+      "@typescript-eslint/no-explicit-any": ["warn"],
+      "@typescript-eslint/no-non-null-asserted-optional-chain": ["warn"]
     },
   }),
 ];
