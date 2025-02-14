851793079a750746f2bc7df097072c89e66b2ac3
Sushant Gandhi <sushant@simplified.co>
2025-02-14 10:19:38 +0530

    switched off unused-var eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     plugins: ["@typescript-eslint"],
     rules: {
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-unused-vars": ["off"],
     },
   }),
 ];
