cc7598e1a9a144dc6135b7eee5bebe65fd1ae185
Sushant Gandhi <sushant@simplified.co>
2025-02-14 13:08:54 +0530

    added layer options in layers sidebar

--- src/features/editor/components/layers-sidebar.tsx
+++ src/features/editor/components/layers-sidebar.tsx
@@ -8,7 +8,39 @@ import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-clos
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 import { isImageType, isTextType } from "../utils";
-import { SearchIcon } from "lucide-react";
+import {
+  AlignCenterHorizontalIcon,
+  AlignCenterVerticalIcon,
+  AlignEndHorizontalIcon,
+  AlignEndVerticalIcon,
+  AlignStartHorizontalIcon,
+  AlignStartVerticalIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  BringToFrontIcon,
+  ClipboardIcon,
+  CopyIcon,
+  CopyPlusIcon,
+  LayersIcon,
+  LockIcon,
+  MoreHorizontalIcon,
+  SearchIcon,
+  SendToBackIcon,
+  TrashIcon,
+  UnlockIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface LayersSidebarProps {
   activeTool: ActiveTool;
@@ -69,84 +101,242 @@ const LayersSidebar = ({
             const rectLayer = layer?.type === "rect";
 
             return (
-              <Button
-                key={index}
-                variant="secondary"
-                size="lg"
-                onClick={() => {
-                  editor?.canvas.setActiveObject(layer);
-                  editor?.canvas.renderAll();
-                }}
-                className={cn(
-                  "py-2 px-4 w-full h-16 border-2 border-transparent hover:border-border relative",
-                  foundLayer === layer && "border-2 border-blue-500"
-                )}
-              >
-                {textLayer && (
-                  <span
-                    className="max-w-[240px] truncate"
-                    style={{
-                      fontWeight: textLayer?.fontWeight,
-                      fontStyle: textLayer?.fontStyle,
-                      fontFamily: textLayer?.fontFamily,
-                      color: textLayer?.fill as string,
-                      textDecoration: `${hasUnderline ? "underline" : ""} ${
-                        hasLinethrough ? "line-through" : ""
-                      }`,
-                      transform: `rotate(${layer?.angle}deg)`,
+              <div key={index} className="relative group">
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  onClick={() => {
+                    editor?.canvas.setActiveObject(layer);
+                    editor?.canvas.renderAll();
+                  }}
+                  className={cn(
+                    "py-2 px-4 w-full h-16 border-2 border-transparent hover:border-border",
+                    foundLayer === layer && "border-2 border-blue-500"
+                  )}
+                  disabled={!layer?.selectable}
+                >
+                  {textLayer && (
+                    <span
+                      className="max-w-[240px] truncate"
+                      style={{
+                        fontWeight: textLayer?.fontWeight,
+                        fontStyle: textLayer?.fontStyle,
+                        fontFamily: textLayer?.fontFamily,
+                        color: textLayer?.fill as string,
+                        textDecoration: `${hasUnderline ? "underline" : ""} ${
+                          hasLinethrough ? "line-through" : ""
+                        }`,
+                        transform: `rotate(${layer?.angle}deg)`,
+                      }}
+                    >
+                      {(layer as fabric.FabricText).text}
+                    </span>
+                  )}
+
+                  {imageLayer && (
+                    <Image
+                      // @ts-expect-error layer._originalElement.currentSrc exists
+                      src={layer?._originalElement?.currentSrc}
+                      height={36}
+                      width={36}
+                      alt="Image"
+                      style={{
+                        transform: `rotate(${layer?.angle}deg)`,
+                      }}
+                    />
+                  )}
+
+                  {circleLayer && (
+                    <div
+                      className="h-8 w-8 rounded-full"
+                      style={{
+                        backgroundColor: layer?.fill as string,
+                        borderWidth: `${layer?.strokeWidth * 0.1}px`,
+                        borderColor: layer?.stroke as string,
+                      }}
+                    />
+                  )}
+
+                  {rectLayer && (
+                    <div
+                      className="h-8 w-8"
+                      style={{
+                        borderRadius: `${(layer as fabric.Rect)?.rx * 0.1}px`,
+                        backgroundColor: layer?.fill as string,
+                        borderWidth: `${layer?.strokeWidth * 0.1}px`,
+                        borderColor: layer?.stroke as string,
+                        transform: `rotate(${layer?.angle}deg)`,
+                      }}
+                    />
+                  )}
+
+                  {triangleLayer && (
+                    <div
+                      className="h-0 w-0 border-x-[1rem] border-x-transparent border-b-[2rem]"
+                      style={{
+                        borderBottomColor: layer?.fill as string,
+                        transform: `rotate(${layer?.angle}deg)`,
+                      }}
+                    />
+                  )}
+                </Button>
+
+                {layer.selectable && (
+                  <DropdownMenu
+                    onOpenChange={(open) => {
+                      if (open) {
+                        editor?.canvas.setActiveObject(layer);
+                        editor?.canvas.renderAll();
+                      }
                     }}
                   >
-                    {(layer as fabric.FabricText).text}
-                  </span>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        size="icon"
+                        variant="secondary"
+                        className="absolute top-1 right-1 opacity-0 group-hover:opacity-100"
+                      >
+                        <MoreHorizontalIcon />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuItem onClick={() => editor?.onCopy()}>
+                        <CopyIcon />
+                        Copy
+                        <DropdownMenuShortcut>⌘C</DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => editor?.onPaste()}>
+                        <ClipboardIcon />
+                        Paste
+                        <DropdownMenuShortcut>⌘V</DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={async () => {
+                          await editor?.onCopy();
+                          editor?.onPaste();
+                        }}
+                      >
+                        <CopyPlusIcon />
+                        Duplicate
+                        <DropdownMenuShortcut>⌘D</DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => editor?.delete()}>
+                        <TrashIcon />
+                        Delete
+                        <DropdownMenuShortcut>Del</DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                          <LayersIcon />
+                          Layer
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem
+                              onClick={() => editor?.bringForward()}
+                            >
+                              <ArrowUpIcon />
+                              Bring forward
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.sendBackwards()}
+                            >
+                              <ArrowDownIcon />
+                              Send backward
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.bringToFront()}
+                            >
+                              <BringToFrontIcon />
+                              Bring front
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.sendToBack()}
+                            >
+                              <SendToBackIcon />
+                              Send back
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                          <AlignStartVerticalIcon />
+                          Align
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem
+                              onClick={() => editor?.changeAlignment("left")}
+                            >
+                              <AlignStartVerticalIcon />
+                              Left
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.changeAlignment("center")}
+                            >
+                              <AlignCenterVerticalIcon />
+                              Center
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.changeAlignment("right")}
+                            >
+                              <AlignEndVerticalIcon />
+                              Right
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem
+                              onClick={() => editor?.changeAlignment("top")}
+                            >
+                              <AlignStartHorizontalIcon />
+                              Top
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.changeAlignment("middle")}
+                            >
+                              <AlignCenterHorizontalIcon />
+                              Middle
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => editor?.changeAlignment("bottom")}
+                            >
+                              <AlignEndHorizontalIcon />
+                              Bottom
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem
+                        onClick={() => {
+                          editor?.changeSelectable(false);
+                          editor?.canvas.discardActiveObject();
+                          editor?.canvas.renderAll();
+                        }}
+                      >
+                        <LockIcon />
+                        Lock
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 )}
 
-                {imageLayer && (
-                  <Image
-                    // @ts-expect-error layer._originalElement.currentSrc exists
-                    src={layer?._originalElement?.currentSrc}
-                    height={36}
-                    width={36}
-                    alt="Image"
-                    style={{
-                      transform: `rotate(${layer?.angle}deg)`,
+                {!layer.selectable && (
+                  <Button
+                    size="icon"
+                    variant="secondary"
+                    className="absolute top-1/2 right-2 -translate-y-1/2 opacity-0 group-hover:opacity-100"
+                    onClick={() => {
+                      editor?.changeSelectable(true);
+                      editor?.canvas.setActiveObject(layer);
+                      editor?.canvas.renderAll();
                     }}
-                  />
+                  >
+                    <UnlockIcon />
+                  </Button>
                 )}
-
-                {circleLayer && (
-                  <div
-                    className="h-8 w-8 rounded-full"
-                    style={{
-                      backgroundColor: layer?.fill as string,
-                      borderWidth: `${layer?.strokeWidth * 0.1}px`,
-                      borderColor: layer?.stroke as string,
-                    }}
-                  />
-                )}
-
-                {rectLayer && (
-                  <div
-                    className="h-8 w-8"
-                    style={{
-                      borderRadius: `${(layer as fabric.Rect)?.rx * 0.1}px`,
-                      backgroundColor: layer?.fill as string,
-                      borderWidth: `${layer?.strokeWidth * 0.1}px`,
-                      borderColor: layer?.stroke as string,
-                      transform: `rotate(${layer?.angle}deg)`,
-                    }}
-                  />
-                )}
-
-                {triangleLayer && (
-                  <div
-                    className="h-0 w-0 border-x-[1rem] border-x-transparent border-b-[2rem]"
-                    style={{
-                      borderBottomColor: layer?.fill as string,
-                      transform: `rotate(${layer?.angle}deg)`,
-                    }}
-                  />
-                )}
-              </Button>
+              </div>
             );
           })}
         </div>
